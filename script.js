ba735aac8256415e60221c98366313763f5a3dab
unknown <sabu0550@gmail.com>
2022-09-21 08:13:01 +0200

    fixed everything and add removed

--- script.js
+++ script.js
@@ -1,69 +1,70 @@
 
 // PRODUCTS VARIABLE
 var productsState = [
+  
   {
     id: 1,
-    name: 'Macbook Pro',
-    price: 21000,
+    name: 'Samsung Galaxy Z Flip 3',
+    price: 20999,
     rates: 5,
     image:
-      'https://www.cnet.com/a/img/resize/4e82f3a17554a5aff8089194237de5a3acfce3b4/2022/04/27/b796792b-5b34-4405-83eb-efc66371ee06/samsung-galaxy-book-2-pro-360-01.jpg?auto=webp&fit=crop&height=630&width=1200',
+      'https://images.techadvisor.com/cmsdata/deal/3807448/galaxy_z_flip3__review_14.jpg?quality=50&strip=all',
   },
   {
-    id: 2,
+    id: 1,
     name: 'Iphone 13 Pro',
-    price: 23000,
-    rates: 3,
+    price: 22999,
+    rates: 4,
     image:
       'https://imageio.forbes.com/specials-images/imageserve/6148e5c7b85bfa189955ed22/0x0.jpg?format=jpg&width=1200',
   },
   {
     id: 1,
     name: 'Iphone 6 C',
-    price: 7000,
-    rates: 2,
+    price: 5999,
+    rates: 4,
     image:
       'https://images.unsplash.com/photo-1565849904461-04a58ad377e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80',
   },
   {
     id: 1,
-    name: 'HP Laptop',
-    price: 8000,
-    rates: 1,
-    image:
-      'https://images.techadvisor.com/cmsdata/reviews/3803826/hp_pavilion_14_laptop02_thumb.jpg',
-  },
-  {
-    id: 1,
-    name: 'Dell Laptop',
-    price: 4000,
-    rates: 1,
-    image:
-      'https://images.unsplash.com/photo-1558864559-ed673ba3610b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGRlbGwlMjBsYXB0b3B8ZW58MHx8MHx8&w=1000&q=80',
-  },
-  {
-    id: 1,
-    name: 'Macbook Pro',
-    price: 15000,
+    name: 'Samsung galaxy fold 4',
+    price: 36999,
     rates: 5,
     image:
-      'https://images.idgesg.net/images/idge/imported/imageapi/2021/10/18/20/14-16-inch-macbook-pro-2021-100907465-large.jpg?auto=webp&quality=85,70',
+      'https://images.samsung.com/za/smartphones/galaxy-z-fold4/images/galaxy-z-fold4_highlights_kv.jpg',
   },
   {
     id: 1,
-    name: 'Playstation 5',
-    price: 20000,
+    name: 'huawei p50 pro',
+    price: 14499,
     rates: 4,
     image:
-      'https://wallpaperaccess.com/full/3012890.jpg',
+      'https://snapcraze.co.za/wp-content/uploads/2022/05/p50-pro-black.jpeg',
   },
   {
     id: 1,
-    name: 'PS 4 Joystick',
-    price: 1200,
-    rates: 3,
+    name: 'honor magic 4 pro',
+    price: 22499,
+    rates: 5,
     image:
-      'https://wallpaper.dog/large/20520129.jpg',
+      'https://www.hihonor.com/content/dam/honor/za/products/smartphone/honor-magic4-pro/lange/honor-magic4-facebook-shareimg.jpg',
+  },
+  {
+    id: 1,
+    name: 'nokia x50 pro',
+    price: 32000,
+    rates: 4,
+    image:
+      'https://www.pricepony.com.ph/blog/wp-content/uploads/2021/06/H6c875277bb864fa383b1adc7e8662b5fw.jpg',
+  },
+  {
+    id: 1,
+    name: 'oppo reno 7',
+    price: 17999,
+    rates: 4,
+    image:
+      'https://www.greenbot.com/wp-content/uploads/2021/11/oppo-reno-7-pro-featured.png',
   },
 ]
 
@@ -73,8 +74,8 @@ var tableCart = []
 
 // LINK JS TO HTML ELEMENT
 const products = document.getElementById('products')
-const cartShopping = document.getElementById('cartShopping')
-const prodD = document.getElementById('prodD')
+const cartShopping = document.getElementById('cartShopping');
+const prodDisplay = document.getElementById('prodDisplay');
 
 
 
@@ -82,69 +83,93 @@ const prodD = document.getElementById('prodD')
 function DataDisplayProducts() {
   products.innerHTML = ""
   // loop into productsState and display
-  for (let s = 0; s < productsState.length; s++) {
+  for (let i = 0; i < productsState.length; i++) {
     products.innerHTML += `
       
       <div class="product">
           <div class="product__img">
               <img
-                src=${productsState[s].image}
+                src=${productsState[i].image}
                 alt=""
               />
           </div>
-              <div class="product__name">${productsState[s].name}</div>
+              <div class="product__name">${productsState[i].name}</div>
                 <div class="product__rate">
-                  ${'<span>*</span>'.repeat(productsState[s].rates)}
+                  ${'<span>*</span>'.repeat(productsState[i].rates)}
                 </div>
-                <div class="product__price">R <span>${productsState[s].price}</span></div> 
-                  <button onclick="online(${s})">+ ADD TO CART</button> 
+                <div class="product__price">R <span>${productsState[i].price}</span></div> 
+                  <button onclick="online(${i})">+ ADD TO CART</button>
             </div>
       
       `
 
   }
-  cartShopping.innerHTML = tableCart.length;
 
  
-
-
 }
 
-// DISPLAY INTO FUNCTION
-function online(s) {
+// add function for removed
+function removeClear(i) {
+  tableCart.splice(i,1)
+  
+  prodD()
 
-  const prodD = document.getElementById('prodDisplay');
-  document.getElementById('cartShopping').innerHTML = tableCart.length;
-  tableCart.push(productsState[s])
+  DataDisplayProducts()
+  
+}
+
+function totalCash() {
+  var total = 0;
 
   for (let i = 0; i < tableCart.length; i++) {
+        {
+          total += tableCart[i];
+        }
+        return total;
+    
+  }
+}
 
-    prodD.innerHTML += `
+// add function online
+function online(i) {
+  tableCart.push(productsState[i])
+  document.getElementById('cartShopping').innerHTML = tableCart.length;
+
+  prodD()
+
+  DataDisplayProducts()
+}
+
+// add FUNCTION and Display
+function prodD() {
+  prodDisplay.innerHTML = ""
+
+  for (let i = 0; i < tableCart.length; i++) {
+    prodDisplay.innerHTML += `
       
     <div class="product">
         <div class="product__img">
             <img
-              src=${productsState[s].image}
+              src=${tableCart[i].image}
               alt=""
             />
         </div>
-            <div class="product__name">${productsState[s].name}</div>
+            <div class="product__name">${tableCart[i].name}</div>
               <div class="product__rate">
-                ${'<span>*</span>'.repeat(productsState[s].rates)}
+                ${'<span>*</span>'.repeat(tableCart[i].rates)}
               </div>
-              <div class="product__price">R <span>${productsState[s].price}</span></div> 
-                <button onclick="online(${s})">+ ADD TO CART</button> 
+              <div class="product__price">R <span>${tableCart[i].price}</span></div> 
+                <button class="btn btn-danger" onclick="removeClear(${i})">X Clear</button> 
           </div>
     
     `
     
   }
-  cartShopping.innerHTML = tableCart.length;
-
-
+  
 
 }
 
+prodD()
 
 // CALL THE DISPLAY FUNCTION
 DataDisplayProducts()
